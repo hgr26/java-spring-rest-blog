89b84185f0ae10a817ed409ff1ea93e4d94d0f0b
hgr26 <hannah.richardson91@outlook.com>
2021-08-10 11:52:52 +1000

    module 2

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -25,6 +25,9 @@ public class Post {
     @CreationTimestamp
     private Date date;
 
+    @ManyToOne (fetch = FetchType.EAGER)
+    private Author author;
+
     public Post() {
         super();
     }
@@ -72,6 +75,14 @@ public class Post {
         this.date = date;
     }
 
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Post))
